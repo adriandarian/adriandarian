a08de26b93912e9fc35c7c7b9b32cc2a216f3498
Adrian Darian <adarian@ucmerced.edu>
2020-11-23 22:26:54 -0800

    add: web vitals config

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import { Router, Switch, Route } from 'react-router';
 
 import Home from './pages/Home/Home';
 import NotFound from './pages/NotFound/NotFound';
+import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
 import './index.css';
 
@@ -26,3 +28,13 @@ const App: FC = (): JSX.Element => {
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);
