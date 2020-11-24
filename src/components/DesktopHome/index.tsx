537abd0fc0295b2e7af0bb7224577f2e09a4c178
Adrian Darian <adarian@ucmerced.edu>
2020-11-23 20:56:46 -0800

    add: Mobile and Desktop Home templates

---
+++ src/components/DesktopHome/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DesktopHome: React.FC = () => {
+    return <div>Desktop</div>;
+}
+
+export default DesktopHome;
---
+++ src/components/MobileHome/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MobileHome: React.FC = () => {
+    return <div>Mobile</div>;
+}
+
+export default MobileHome;
